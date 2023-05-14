a3dc0ff8abfc08f8646887c60050631df8a2d79a
Szymon Bartkowiak <szymon.b4310@gmail.com>
2023-05-14 10:17:14 +0200

    "dodanie wczytania z pliku"

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,26 @@
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int wyb;
+        ArrayList<String> pytania = new ArrayList<String>();
+
+
         System.out.println("Witaj w moim quizie!");
+        System.out.println("Na ile pytań chcesz odpowiedzieć? (1-10)");
+
+        do {
+            wyb = sc.nextInt();
+            if(wyb < 0 || wyb > 10) {
+                System.out.println("Podales liczbe poza zakresem, sprobuj jeszcze raz");
+            }
+        }while((wyb < 0 || wyb > 10));
+
+
+
 
     }
 }
---
+++ src/wczytaniePytan.java
@@ -0,0 +1,27 @@
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.io.FileNotFoundException;
+
+public class wczytaniePytan {
+    private  ArrayList<String> pyt = new ArrayList<String>();
+
+    public void wczytaj() throws FileNotFoundException {
+        File file = new File("pytania.txt");
+        Scanner scanner = new Scanner(file);
+        int licznikLini = 0;
+        String pytanie;
+
+        while (scanner.hasNext()) {
+            pytanie = scanner.next();
+            if(licznikLini % 7 == 0) {
+                pyt.add(pytanie);
+            }
+            licznikLini++;
+        }
+    }
+
+    public ArrayList<String> getPytania() {
+        return pyt;
+    }
+}
